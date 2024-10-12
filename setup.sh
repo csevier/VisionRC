cfc7dd6b1c15bf1981ca79ec0c0ef7a0429e0488
Cody Sevier <sevierit@gmail.com>
2024-10-12 09:56:31 -0500

    setup script dependencies linux

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt install libsdl2-dev
+sudo apt update && sudo apt install -y cmake g++ wget unzip
+sudo apt-get install -y libopencv-dev
+sudo apt-get install -y libxcb-cursor-dev
+sudo apt-get install build-essential libgl1-mesa-dev
